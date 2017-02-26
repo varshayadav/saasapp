56ad82d1c1e364b751fcff40806a1db5bfbcded8
varshayadav <varshayadav257@gmail.com>
2017-02-26 21:43:59 +0530

    added custom route for new contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,11 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to new_contact_path, notice: 'Successfully created'
+      flash[:success] = 'Saved successfully'
+      redirect_to new_contact_path
     else
-      redirect_to new_contact_path, notice: 'Error occured'
+      flash[:error] = @contact.errors.full_messages.join(', ')
+      redirect_to new_contact_path
     end
   end
 
