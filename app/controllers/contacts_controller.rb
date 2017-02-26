0e64c17d48c274fd2c3cc1c9e0b123709c94e140
varshayadav <varshayadav257@gmail.com>
2017-02-27 01:39:08 +0530

    Mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,10 +8,9 @@ class ContactsController < ApplicationController
     if @contact.save
       name = params[:contact][:name]
       email = params[:contact][:email]
-      body = params[:contact][:comment]
+      body = params[:contact][:comments]
       ContactMailer.contact_email(name, email, body).deliver
-
-      flash[:success] = 'Saved successfully'
+      flash[:success] = 'Message Sent'
       redirect_to new_contact_path
     else
       flash[:error] = @contact.errors.full_messages.join(', ')
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default: 'varshayadav257@gmail.com'
+  default to: 'varshayadav257@gmail.com'
 
   def contact_email(name, email, body)
     @name = name
